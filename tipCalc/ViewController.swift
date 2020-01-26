7217dbcac5be8cf39decd945cc52f4e94607880c
Paul Whipp <pwhipp@csumb.edu>
2020-01-26 14:26:13 -0800

    Initial Commit

---
+++ tipCalc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tipCalc
+//
+//  Created by user163799 on 1/26/20.
+//  Copyright © 2020 whipp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
